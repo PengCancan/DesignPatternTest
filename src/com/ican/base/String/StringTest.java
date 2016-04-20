4ee82b71d1e8713a816a8d53c8987e00e84a22e1
vincentpeng007@outlook.com <vincentpeng007@outlook.com>
2016-04-20 12:19:19 +0800

    mod ==== by peng

--- src/com/ican/base/String/StringTest.java
+++ src/com/ican/base/String/StringTest.java
@@ -20,6 +20,7 @@ public class StringTest {
         System.out.println(a==b);
         System.out.println(a==c);
         System.out.println(c==d);
+        System.out.println("=====");
         System.out.println(null == null);
     }
 }
