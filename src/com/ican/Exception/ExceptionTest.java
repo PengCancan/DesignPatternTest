87f7494908078ed4c139f9de97497ea155dec91a
vincentpeng007@outlook.com <vincentpeng007@outlook.com>
2016-04-21 13:31:29 +0800

    mod ==== by peng

---
+++ src/com/ican/Exception/ExceptionTest.java
@@ -0,0 +1,18 @@
+package com.ican.Exception;
+
+/**
+ * Created by flingcoder
+ * Date:4/12/2016
+ * Description:
+ */
+public class ExceptionTest {
+
+    public static void main(String[] args) {
+        long[] l = new long[1000000000];
+        String s = null;
+        if(s==null || s.length()>=0){
+            System.out.println("");
+        }
+    }
+
+}
